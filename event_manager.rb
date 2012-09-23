176f05b3a81b0abf93b975850d6312b9246f6f90
Franklin Webber <franklin.webber@gmail.com>
2012-09-22 23:12:29 -0400

    Print Names method

--- event_manager.rb
+++ event_manager.rb
@@ -8,7 +8,14 @@ class EventManager
     filename = "event_attendees.csv"
     @file = CSV.open(filename)
   end
+  
+  def print_names
+    @file.each do |line|
+      puts line.inspect
+    end
+  end
 end
 
 # Script
 manager = EventManager.new
+manager.print_names
